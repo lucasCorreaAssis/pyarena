741b9171ae842c125001bbf82aaba204ef47a3c4
Lucas Correa de Assis <lucascorreadeassis@gmail.com>
2022-11-23 20:54:52 -0300

    improved oee subscriber

--- oee_subscriber.py
+++ oee_subscriber.py
@@ -14,23 +14,37 @@ class OeeSubscriber:
         self._threads = []
 
     def start(self):
-        # Still need to implement all subscribers to estimate oee
+        parts_subscriber = self._subscribe_to_parts()
+        # Implement remaining subscribers
+        # * Tempo parado
+        self._threads.append(parts_subscriber)
+
+    def _subscribe_to_parts(self) -> MqttSubscriber:
         parts_config = MqttConfig('parts', self._config.broker, self._config.port)
         parts_subscriber = MqttSubscriber(parts_config)
         parts_subscriber.connect()
         parts_subscriber.subscribe(self._on_message_parts)
         parts_subscriber.start()
-        self._threads.append(parts_subscriber)
+        return parts_subscriber
 
-    # Still need to improve http post!!!
+    def stop(self):
+        for thread in self._threads:
+            thread.stop()
+            thread.join()
+
     def _on_message_parts(self, client, userdata, msg: MQTTMessage):
-        connection = http.client.HTTPSConnection('api.powerbi.com')
-
-        headers = {'Content-type': 'application/json'}
+        # NEED TO ESTIMATE KPIs
 
         qualidade = random.uniform(75, 85)
         disponibilidade = random.uniform(75, 85)
         producao = random.uniform(75, 85)
+
+        self._update_oee(qualidade, disponibilidade, producao)
+
+    def _update_oee(self, qualidade: float, disponibilidade: float, producao: float):
+        connection = http.client.HTTPSConnection('api.powerbi.com')
+
+        headers = {'Content-type': 'application/json'}
 
         body = [
             {
@@ -46,7 +60,4 @@ class OeeSubscriber:
 
         connection.request('POST', f'/beta/8a1ef6c3-8324-4103-bf4a-1328c5dc3653/datasets/00efeb83-e802-4be0-a091-85347beddc60/rows?key=Fa2TgypYwwtqLESfXaXe1op7d9sSATOOBPMBoQbar%2BJ2Xhr%2BxGcPG3O4Tuei%2FnZqMrnH9CUgou6p4il8E7PoWA%3D%3D', json_body, headers)
 
-    def stop(self):
-        for thread in self._threads:
-            thread.stop()
-            thread.join()
+        connection.getresponse()
