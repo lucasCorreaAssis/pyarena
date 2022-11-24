23064162534f380d91a2378399d6b64b886872c1
Adrian Bestel <bestel.adrian@pucpr.edu.br>
2022-11-23 22:01:04 -0300

    create update and calc on OEE

--- oee_subscriber.py
+++ oee_subscriber.py
@@ -8,50 +8,85 @@ import random
 class OeeSubscriber:
     _config: MqttConfig
     _threads: List[MqttSubscriber]
+    _total_parts: int
+    _total_stop_time: float
+    _total_good_parts: int
+    _IDEAL_PRODUCTION: int
+    _WORK_TIME: float
+
 
     def __init__(self, config: MqttConfig):
         self._config = config
         self._threads = []
+        self._total_parts = 0
+        self._total_stop_time = 0.0
+        self._IDEAL_PRODUCTION = 100 # TODO
+        self._WORK_TIME = 1000 # TODO
+        self._total_good_parts = 0       
 
     def start(self):
-        parts_subscriber = self._subscribe_to_parts()
-        # Implement remaining subscribers
-        # * Tempo parado
-        self._threads.append(parts_subscriber)
+        good_parts_subscriber = self._subscribe_to_good_parts()
+        bad_parts_subscriber = self._subscribe_to_bad_parts()
+        fault_subscriber = self._subscribe_to_faults()
+        self._threads.append(good_parts_subscriber, bad_parts_subscriber, fault_subscriber)
 
-    def _subscribe_to_parts(self) -> MqttSubscriber:
-        parts_config = MqttConfig('parts', self._config.broker, self._config.port)
-        parts_subscriber = MqttSubscriber(parts_config)
-        parts_subscriber.connect()
-        parts_subscriber.subscribe(self._on_message_parts)
-        parts_subscriber.start()
-        return parts_subscriber
+    def _subscribe_to_faults(self):
+        fault_config = MqttConfig('fault', self._config.broker, self._config.port)
+        fault_subscriber = MqttSubscriber(fault_config)
+        fault_subscriber.connect()
+        fault_subscriber.subscribe(self._on_message_parts)
+        fault_subscriber.start()
+        return fault_subscriber
+
+    def _subscribe_to_bad_parts(self) -> MqttSubscriber:
+        bad_config = MqttConfig('bad_parts', self._config.broker, self._config.port)
+        bad_subscriber = MqttSubscriber(bad_config)
+        bad_subscriber.connect()
+        bad_subscriber.subscribe(self._on_message_parts)
+        bad_subscriber.start()
+        return bad_subscriber
+
+    def _subscribe_to_good_parts(self) -> MqttSubscriber:
+        good_config = MqttConfig('good_parts', self._config.broker, self._config.port)
+        good_subscriber = MqttSubscriber(good_config)
+        good_subscriber.connect()
+        good_subscriber.subscribe(self._on_message_parts)
+        good_subscriber.start()
+        return good_subscriber
 
     def stop(self):
         for thread in self._threads:
             thread.stop()
             thread.join()
 
-    def _on_message_parts(self, client, userdata, msg: MQTTMessage):
-        # NEED TO ESTIMATE KPIs
+    def _on_message_good(self, client, userdata, msg: MQTTMessage):
+        self._total_good_parts += 1
+        self._total_parts += 1
+        self._update_oee()
+ 
+    def _on_message_bad(self, client, userdata, msg: MQTTMessage):        
+        self._total_parts += 1
+        self._update_oee()
 
-        qualidade = random.uniform(75, 85)
-        disponibilidade = random.uniform(75, 85)
-        producao = random.uniform(75, 85)
+    def _on_message_fault(self, client, userdata, msg: MQTTMessage):
+        self._total_stop_time += float(msg.payload)
+        self._update_oee()
 
-        self._update_oee(qualidade, disponibilidade, producao)
+    def _update_oee(self):
+        quality = self._total_good_parts / self. self._total_parts
+        availability = (self._WORK_TIME - self._total_stop_time) / self._WORK_TIME
+        production = random.uniform(75, 85)
 
-    def _update_oee(self, qualidade: float, disponibilidade: float, producao: float):
         connection = http.client.HTTPSConnection('api.powerbi.com')
 
         headers = {'Content-type': 'application/json'}
 
         body = [
             {
-                "oee" : producao*qualidade*disponibilidade/100**2,
-                "qualidade" :qualidade,
-                "disponibilidade" :disponibilidade,
-                "producao" :producao,
+                "oee" : production*quality*availability/100**2,
+                "qualidade" :quality,
+                "disponibilidade" :availability,
+                "producao" :production,
                 "min": 0,
                 "max": 100
             }
